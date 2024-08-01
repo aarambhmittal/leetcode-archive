ce3da7ce4631a546f2f2355ea7c9ea866128c0e7
aarambhmittal <aarambhmittal@gmail.com>
2024-08-01 07:15:17 +0530

    Update 1105. Filling Bookcase Shelves.cpp

--- problems/1001 - 2000/1101 - 1200/1105. Filling Bookcase Shelves.cpp
+++ problems/1001 - 2000/1101 - 1200/1105. Filling Bookcase Shelves.cpp
@@ -1,10 +1,11 @@
+// top-down
 class Solution {
 private:
     int32_t n;
     std::vector<std::vector<int32_t>> memo;
     
 public:
-    int32_t minHeightShelves(std::vector<std::vector<int32_t>>& books, int shelfWidth) {
+    int32_t minHeightShelves(std::vector<std::vector<int32_t>>& books, int32_t shelfWidth) {
         n = books.size();
         memo.assign(n, std::vector<int32_t>(shelfWidth + 1, 0));
         
@@ -26,5 +27,35 @@ public:
             return memo[i][remWidth]; 
         };
         return helper(0, shelfWidth, 0);
+    }
+};
+
+// bottom-up
+class Solution {
+private:
+    int32_t n;
+    std::vector<int32_t> dp;
+    
+public:
+    int32_t minHeightShelves(std::vector<std::vector<int32_t>>& books, int32_t shelfWidth) {
+        n = books.size(); 
+        
+        dp.resize(n + 1);
+        dp[0] = 0; dp[1] = books[0][1] + dp[0];
+
+        for(int32_t i = 2; i < n + 1; i++) {
+            int32_t remWidth = shelfWidth - books[i - 1][0];
+            int32_t curHeight = books[i - 1][1];
+            dp[i] = curHeight + dp[i - 1];
+
+            int32_t j = i - 1;
+            while(j > 0 && (remWidth - books[j - 1][0] >= 0)) {
+                remWidth -= books[j - 1][0];
+                curHeight = std::max(curHeight, books[j - 1][1]);
+                dp[i] = std::min(dp[i], dp[j - 1] + curHeight);
+                j--;
+            }
+        }
+        return dp[n];
     }
 };
